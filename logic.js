015947fe15e99ff549e247ec448288af291cd240
jashandeepvirk <jashan2001virk@gmail.com>
2025-04-08 23:14:58 +0530

    Change Background function in js
    function changes the color of the page background on random basis

--- logic.js
+++ logic.js
@@ -3,3 +3,10 @@ const secondName = "Virk";
 console.log("Welcome to my Portfolio Page");
 const myName = document.getElementById("myName");
 myName.textContent = firstName + " " + secondName;
+function changeBackgroundColor(){
+    var colors = ["red", "blue", "green", "pink", "yellow", "orange"];
+    var randomNumber = Math.floor(Math.random()*colors.length);
+    document.body.style.backgroundColor=colors[randomNumber];
+    console.log("Colors array length: "+colors.length);
+    console.log("Random number: "+randomNumber);
+}
